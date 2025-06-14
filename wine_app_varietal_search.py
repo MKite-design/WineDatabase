b5c37b0e49dbbadf39c5e3f1df47546954dd0cfe
MKite <maximilliankite@gmail.com>
2025-06-15 00:21:10 +1000

    Update wine_app_varietal_search.py

--- wine_app_varietal_search.py
+++ wine_app_varietal_search.py
@@ -74,10 +74,13 @@ with st.container():
 # --- SIDEBAR ADVANCED FILTERS ---
 with st.sidebar:
     st.header("⚙️ Advanced Filters")
+    
     under_50 = st.checkbox("💲 Show only wines under $50")
     over_500 = st.checkbox("💰 Show only wines over $500")
+    
     max_price = float(df["bottle_price"].max()) + 10  # add buffer for slider headroom
     price_min, price_max = st.slider("Price Range",0.0,max_price,(0.0, max_price))
+    
     varietals = st.multiselect("Varietal", sorted(df["varietal"].unique()))
     producers = st.multiselect("Producer", sorted(df["producer"].unique()))
     suppliers = st.multiselect("Supplier", sorted(df["supplier"].unique()))
